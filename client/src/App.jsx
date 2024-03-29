7236d37b04abea21a01e9135ddc64e642e9e522e
unknown <saifkhanali101@gmail.com>
2024-03-30 00:38:33 +0530

    Login-SignUp-done-frontend-Only

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+// const NotFound = lazy(() => import("./pages/NotFound"));
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
