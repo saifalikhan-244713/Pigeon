e42e1c75753c748e4116399c9c22177f40a26ce4
unknown <saifkhanali101@gmail.com>
2024-04-06 03:09:28 +0530

    15th

--- client/src/pages/admin/MessageManagement.jsx
+++ client/src/pages/admin/MessageManagement.jsx
@@ -1,5 +1,14 @@
-import { Avatar } from "@mui/material";
+import { useFetchData } from "6pp";
+import { Avatar, Box, Stack } from "@mui/material";
+import moment from "moment";
+import { useEffect, useState } from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
+import RenderAttachment from "../../components/shared/RenderAttachment";
+import Table from "../../components/shared/Table";
+import { server } from "../../constants/config";
+// import { useErrors } from "../../hooks/hook";
+import { fileFormat, transformImage } from "../../lib/features";
+import { dashboardData } from "../../constants/sampleData";
 
 const columns = [
   {
@@ -9,45 +18,124 @@ const columns = [
     width: 200,
   },
   {
-    field: "avatar",
-    headerName: "Avatar",
+    field: "attachments",
+    headerName: "Attachments",
     headerClassName: "table-header",
-    width: 150,
-    renderCell: (params) => (
-      <Avatar alt={params.row.name} src={params.row.avatar} />
-    ),
+    width: 200,
+    renderCell: (params) => {
+      const { attachments } = params.row;
+      return attachments?.length > 0
+        ? attachments.map((i) => {
+                const url = i.url;
+                const file = fileFormat(url);
+
+
+
+
+
+                return (
+                    <Box>
+                      <a
+                        href={url}
+                        download
+                        target="_blank"
+                        style={{
+                          color: "black",
+                        }}
+                      >
+                        {RenderAttachment(file, url)}
+                      </a>
+                    </Box>
+                  );          })
+        : "no attachments";
+      //     return attachments?.length > 0
+      //       ? attachments.map((i) => {
+
+      //         })
+      //       : "No Attachments";
+      //   },
+      // },
+    },
   },
 
   {
-    field: "name",
-    headerName: "Name",
+    field: "content",
+    headerName: "Content",
     headerClassName: "table-header",
-    width: 200,
+    width: 400,
   },
   {
-    field: "username",
-    headerName: "Username",
+    field: "sender",
+    headerName: "Sent By",
     headerClassName: "table-header",
     width: 200,
+    renderCell: (params) => (
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        <Avatar alt={params.row.sender.name} src={params.row.sender.avatar} />
+        <span>{params.row.sender.name}</span>
+      </Stack>
+    ),
   },
   {
-    field: "friends",
-    headerName: "Friends",
+    field: "chat",
+    headerName: "Chat",
     headerClassName: "table-header",
-    width: 150,
+    width: 220,
   },
   {
-    field: "groups",
-    headerName: "Groups",
+    field: "groupChat",
+    headerName: "Group Chat",
     headerClassName: "table-header",
-    width: 200,
+    width: 100,
+  },
+  {
+    field: "createdAt",
+    headerName: "Time",
+    headerClassName: "table-header",
+    width: 250,
   },
 ];
 
 const MessageManagement = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    // setRows(
+    //   dashboardData.chats.map((i) => ({
+    //     ...i,
+    //     id: i._id,
+    //     avatar: i.avatar.map((avatar) => transformImage(avatar, 50)),
+    //     members: i.members.map((member) => ({
+    //       ...member,
+    //       avatar: transformImage(member.avatar, 50),
+    //     })),
+    //     creator: {
+    //       name: i.creator.name,
+    //       avatar: transformImage(i.creator.avatar, 50),
+    //     },
+    //   }))
+    // );
+    setRows(
+      dashboardData.messages.map((i) => ({
+        ...i,
+        id: i._id,
+        sender: {
+          name: i.sender.name,
+          avatar: transformImage(i.sender.avatar, 50),
+        },
+        ceatedAt: moment(i.createdAt).format("MMM Do YYYY,h:mm:ss a"),
+      }))
+    );
+  }, []);
+
   return (
     <AdminLayout>
-      <div>MessageManagement</div>
+      <Table
+        heading={"All Messages"}
+        columns={columns}
+        rows={rows}
+        rowHeight={200}
+      />{" "}
     </AdminLayout>
   );
 };
